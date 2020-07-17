63879f2fdecd02e3dc5b6e3d568eb7f964f2c787
Yuri Calabrez <yuri.calabrez@gmail.com>
2020-07-16 23:07:57 -0300

    Encodando video

--- application/services/video_service.go
+++ application/services/video_service.go
@@ -22,7 +22,7 @@ func NewVideoService() VideoService {
 
 func (v *VideoService) Download(bucketName string) error {
 
-	cxt := context.BackGround()
+	cxt := context.Background()
 	client, err := storage.NewClient(cxt)
 	if err != nil {
 		return err
@@ -43,12 +43,12 @@ func (v *VideoService) Download(bucketName string) error {
 		return err
 	}
 
-	file, err := os.Create(os.GetEnv("LOCAL_STORAGE_PATH") + "/" + v.Video.ID + ".mp4")
+	file, err := os.Create(os.Getenv("LOCAL_STORAGE_PATH") + "/" + v.Video.ID + ".mp4")
 	if err != nil {
 		return err
 	}
 
-	_, err := file.Write(body)
+	_, err = file.Write(body)
 	if err != nil {
 		return err
 	}
@@ -61,13 +61,13 @@ func (v *VideoService) Download(bucketName string) error {
 }
 
 func (v *VideoService) Fragment() error {
-	err := os.Mkdir(os.GetEnv("LOCAL_STORAGE_PATH") + "/" + v.Video.ID, os.ModePerm)
+	err := os.Mkdir(os.Getenv("LOCAL_STORAGE_PATH") + "/" + v.Video.ID, os.ModePerm)
 	if err != nil {
 		return err
 	}
 
-	source := os.GetEnv("LOCAL_STORAGE_PATH") + "/" + v.Video.ID + ".mp4"
-	target := os.GetEnv("LOCAL_STORAGE_PATH") + "/" + v.Video.ID + ".frag"
+	source := os.Getenv("LOCAL_STORAGE_PATH") + "/" + v.Video.ID + ".mp4"
+	target := os.Getenv("LOCAL_STORAGE_PATH") + "/" + v.Video.ID + ".frag"
 
 	cmd := exec.Command("mp4fragment", source, target)
 	output, err := cmd.CombinedOutput()
@@ -79,6 +79,53 @@ func (v *VideoService) Fragment() error {
 
 	return nil
 
+}
+
+func (v *VideoService) Encode() error {
+	cmdArgs := []string{}
+
+	cmdArgs = append(cmdArgs, os.Getenv("LOCAL_STORAGE_PATH") + "/" + v.Video.ID + ".frag")
+	cmdArgs = append(cmdArgs, "--use-segment-timeline") //picota o video
+	cmdArgs = append(cmdArgs, "-o")
+	cmdArgs = append(cmdArgs, os.Getenv("LOCAL_STORAGE_PATH") + "/" + v.Video.ID)
+	cmdArgs = append(cmdArgs, "-f")
+	cmdArgs = append(cmdArgs, "--exec-dir")
+	cmdArgs = append(cmdArgs, "/opt/bento4/bin/")
+	
+	cmd := exec.Command("mp4dash", cmdArgs...)
+
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return err
+	}
+
+	printOutput(output)
+
+	return nil
+}
+
+func (v *VideoService) Finish() error {
+	err := os.Remove(os.Getenv("LOCAL_STORAGE_PATH") + "/" + v.Video.ID + ".mp4")
+	if err != nil {
+		log.Println("Error removing mp4 ", v.Video.ID, ".mp4")
+		return err
+	}
+
+	err = os.Remove(os.Getenv("LOCAL_STORAGE_PATH") + "/" + v.Video.ID + ".frag")
+	if err != nil {
+		log.Println("Error removing frag ", v.Video.ID, ".frag")
+		return err
+	}
+
+	err = os.RemoveAll(os.Getenv("LOCAL_STORAGE_PATH") + "/" + v.Video.ID + ".mp4")
+	if err != nil {
+		log.Println("Error removing directory ", v.Video.ID)
+		return err
+	}
+
+	log.Println("Files have been removed", v.Video.ID)
+
+	return nil
 }
 
 func printOutput(out []byte) {
